ebbd45353aa2d4519596867552ba24b0353844b2
Aleksandra Marzec <aleks.marzec@gmail.com>
2023-05-13 20:26:55 +0200

    Solved the ATM task (first iteration, probably not the most efficient)

--- src/main/java/io/greentesla/api/AtmsApiController.java
+++ src/main/java/io/greentesla/api/AtmsApiController.java
@@ -3,6 +3,7 @@ package io.greentesla.api;
 import io.greentesla.model.atmservice.Order;
 import io.greentesla.model.atmservice.Task;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.greentesla.service.AtmsService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -36,13 +37,12 @@ public class AtmsApiController implements AtmsApi {
 
     public ResponseEntity<Order> calculate(@Parameter(in = ParameterIn.DEFAULT, description = "", required=true, schema=@Schema()) @Valid @RequestBody List<Task> body) {
         String accept = request.getHeader("Accept");
+        AtmsService logic = new AtmsService();
+
         if (accept != null && accept.contains("application/json")) {
-            try {
-              return new ResponseEntity<Order>(objectMapper.readValue("[ {\n  \"atmId\" : 500,\n  \"region\" : 10\n}, {\n  \"atmId\" : 500,\n  \"region\" : 10\n} ]", Order.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Order>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+            AtmsService atmsService = new AtmsService();
+            Order res = atmsService.solve(body);
+            return new ResponseEntity<Order>(res, HttpStatus.OK);
         }
 
         return new ResponseEntity<Order>(HttpStatus.NOT_IMPLEMENTED);
---
+++ src/main/java/io/greentesla/service/AtmsService.java
@@ -0,0 +1,97 @@
+package io.greentesla.service;
+
+import io.greentesla.model.atmservice.ATM;
+import io.greentesla.model.atmservice.Order;
+import io.greentesla.model.atmservice.Task;
+
+import java.util.*;
+
+public class AtmsService {
+    public Order solve(List<Task> tasks) {
+        HashMap<Integer, List<Task>> groupedTasksByRegion = new HashMap<Integer, List<Task>>();
+        for (Task student: tasks) {
+            int key = student.getRegion();
+            if (groupedTasksByRegion.get(key) == null) {
+                groupedTasksByRegion.put(key, new ArrayList<Task>());
+            }
+            groupedTasksByRegion.get(key).add(student);
+        }
+
+        // you can try to not use hashmap and right away and instead use priorityqueue
+
+        List<Task> sortedTasks = new ArrayList<Task>();
+
+        for (int i = 1; i <= groupedTasksByRegion.size(); i++) {
+            sortedTasks.addAll(sortByPriorityInsideTheRegion(groupedTasksByRegion.get(i)));
+        }
+
+        Order res = new Order();
+        for (Task task : sortedTasks) {
+            ATM atm = new ATM();
+            atm.setAtmId(task.getAtmId());
+            atm.setRegion(task.getRegion());
+            res.add(atm);
+        }
+        return res;
+    }
+
+    private List<Task> sortByPriorityInsideTheRegion(List<Task> tasks) {
+        Map<Integer, Task> tasksMap = new HashMap<>();
+
+        for (Task task : tasks) {
+            if (tasksMap.containsKey(task.getAtmId())) {
+                Task currentMostImportant = tasksMap.get(task.getAtmId());
+
+                if (mapper(task) > mapper(currentMostImportant)) {
+                    tasksMap.put(task.getAtmId(), task);
+                }
+            } else {
+                tasksMap.put(task.getAtmId(), task);
+            }
+        }
+
+        PriorityQueue<Task> queue = new PriorityQueue<>(tasksMap.size(), new TaskComparator());
+        queue.addAll(tasksMap.values());
+
+        return new ArrayList<Task>(queue);
+    }
+
+    private static Integer mapper(Task task) {
+        switch (task.getRequestType()) {
+            case FAILURE_RESTART:
+                return 4;
+            case PRIORITY:
+                return 3;
+            case SIGNAL_LOW:
+                return 2;
+            case STANDARD:
+                return 1;
+            default:
+                // todo you can throw expection
+                return 0;
+        }
+    }
+}
+
+class TaskComparator implements Comparator<Task> {
+    @Override
+    public int compare(Task o1, Task o2) {
+        return mapper(o2) - mapper(o1);
+    }
+
+    private static Integer mapper(Task task) {
+        switch (task.getRequestType()) {
+            case FAILURE_RESTART:
+                return 4;
+            case PRIORITY:
+                return 3;
+            case SIGNAL_LOW:
+                return 2;
+            case STANDARD:
+                return 1;
+            default:
+                // todo you can throw expection
+                return 0;
+        }
+    }
+}
