ee8b17f1de8ec46313ad3654da6e883c9805e527
Aleksandra Marzec <aleks.marzec@gmail.com>
2023-05-14 18:20:45 +0200

    ATM task improvement (#20)
    * Reduced complexity of the existing ATM solution
    
    * Refactor

--- src/main/java/io/greentesla/service/AtmsService.java
+++ src/main/java/io/greentesla/service/AtmsService.java
@@ -8,78 +8,47 @@ import java.util.*;
 
 public class AtmsService {
     public Order solve(List<Task> tasks) {
-        HashMap<Integer, List<Task>> groupedTasksByRegion = new HashMap<Integer, List<Task>>();
-        for (Task student: tasks) {
-            int key = student.getRegion();
-            if (groupedTasksByRegion.get(key) == null) {
-                groupedTasksByRegion.put(key, new ArrayList<Task>());
-            }
-            groupedTasksByRegion.get(key).add(student);
-        }
+        Collections.sort(tasks, new TaskComparator());
+        int currentRegion = tasks.get(0).getRegion();
+        HashSet<Integer> atmIdsAlreadySeen = new HashSet<>();
 
-        // you can try to not use hashmap and right away and instead use priorityqueue
-
-        List<Task> sortedTasks = new ArrayList<Task>();
-
-        for (int i = 1; i <= groupedTasksByRegion.size(); i++) {
-            sortedTasks.addAll(sortByPriorityInsideTheRegion(groupedTasksByRegion.get(i)));
-        }
-
-        Order res = new Order();
-        for (Task task : sortedTasks) {
-            ATM atm = new ATM();
-            atm.setAtmId(task.getAtmId());
-            atm.setRegion(task.getRegion());
-            res.add(atm);
-        }
-        return res;
-    }
-
-    private List<Task> sortByPriorityInsideTheRegion(List<Task> tasks) {
-        Map<Integer, Task> tasksMap = new HashMap<>();
+        List<ATM> atmsOrder = new ArrayList<>();
 
         for (Task task : tasks) {
-            if (tasksMap.containsKey(task.getAtmId())) {
-                Task currentMostImportant = tasksMap.get(task.getAtmId());
+            int taskRegion = task.getRegion();
 
-                if (mapper(task) > mapper(currentMostImportant)) {
-                    tasksMap.put(task.getAtmId(), task);
-                }
+            if (currentRegion != taskRegion) {
+                currentRegion = taskRegion;
+                atmIdsAlreadySeen.clear();
+            }
+
+            if (atmIdsAlreadySeen.contains(task.getAtmId())) {
+                continue;
             } else {
-                tasksMap.put(task.getAtmId(), task);
+                atmIdsAlreadySeen.add(task.getAtmId());
+                ATM atm = new ATM();
+                atm.setAtmId(task.getAtmId());
+                atm.setRegion(task.getRegion());
+                atmsOrder.add(atm);
             }
         }
 
-        PriorityQueue<Task> queue = new PriorityQueue<>(tasksMap.size(), new TaskComparator());
-        queue.addAll(tasksMap.values());
-
-        return new ArrayList<Task>(queue);
-    }
-
-    private static Integer mapper(Task task) {
-        switch (task.getRequestType()) {
-            case FAILURE_RESTART:
-                return 4;
-            case PRIORITY:
-                return 3;
-            case SIGNAL_LOW:
-                return 2;
-            case STANDARD:
-                return 1;
-            default:
-                // todo you can throw expection
-                return 0;
-        }
+        Order order = new Order();
+        order.addAll(atmsOrder);
+        return order;
     }
 }
 
 class TaskComparator implements Comparator<Task> {
     @Override
     public int compare(Task o1, Task o2) {
-        return mapper(o2) - mapper(o1);
+        if (o1.getRegion() != o2.getRegion()) {
+            return o1.getRegion() - o2.getRegion();
+        }
+        return priorityMapper(o2) - priorityMapper(o1);
     }
 
-    private static Integer mapper(Task task) {
+    private static Integer priorityMapper(Task task) {
         switch (task.getRequestType()) {
             case FAILURE_RESTART:
                 return 4;
@@ -90,7 +59,6 @@ class TaskComparator implements Comparator<Task> {
             case STANDARD:
                 return 1;
             default:
-                // todo you can throw expection
                 return 0;
         }
     }
